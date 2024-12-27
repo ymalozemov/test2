34fb2cce9278eb6653b31a75637ff90ae3e6e420
yaroslavmalozemov <malozemovyaroslav@gmail.com>
2024-12-27 23:08:41 +0300

    feat(app): init player animation

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,27 +1,24 @@
 using UnityEngine;
 
-namespace Camera
+public class CameraFollow : MonoBehaviour
 {
-    public class CameraFollow : MonoBehaviour
+    public Transform target; // Персонаж, за которым будет следить камера
+    public Vector3 offset; // Смещение камеры относительно персонажа
+
+    private void Start()
     {
-        public Transform target; // Персонаж, за которым будет следить камера
-        public Vector3 offset; // Смещение камеры относительно персонажа
+        target = FindFirstObjectByType<Player.PlayerMovement>().transform;
 
-        private void Start()
+        // Установите смещение, если оно не задано
+        if (offset == Vector3.zero)
         {
-            target = FindFirstObjectByType<Player.PlayerMovement>().transform;
-
-            // Установите смещение, если оно не задано
-            if (offset == Vector3.zero)
-            {
-                offset = new Vector3(0, 5, -10); // Пример смещения для изометрической камеры
-            }
+            offset = new Vector3(0, 5, -10); // Пример смещения для изометрической камеры
         }
+    }
 
-        private void LateUpdate()
-        {
-            Vector3 desiredPosition = target.position + offset;
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5f);
-        }
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5f);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,60 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        public float moveSpeed = 5f;
-
+        private float _moveSpeed = 1f;
+        private float _turnSpeed = 700f;
+        private float _transitionTime = 1f; 
+        private Animator _animator;
+        private int _isStandingHash;
+        private int _isWalkingHash;
+        private int _isRuningHash;
+        private Rigidbody _rb; // Ссылка на компонент Rigidbody
+        private float _timer;
+        
+        void Start()
+        {
+            _isStandingHash = Animator.StringToHash("isStanding");
+            _isWalkingHash = Animator.StringToHash("isWalk");
+            _isRuningHash = Animator.StringToHash("isRun");
+            _animator = GetComponent<Animator>();
+            _animator.SetBool(_isStandingHash, true);
+            _timer = 0f;
+        }
+        
         private void Update()
         {
-            var moveHorizontal = Input.GetAxis("Horizontal");
-            var moveVertical = Input.GetAxis("Vertical");
-            var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            var deltaTime = Time.deltaTime;
-            var deltaSpeed = moveSpeed * deltaTime;
-            transform.position += movement * deltaSpeed;
+            
+            var moveHorizontal = Input.GetAxis("Horizontal"); // A/D или стрелки влево/вправо
+            var moveVertical = Input.GetAxis("Vertical"); // W/S или стрелки вверх/вниз
+            var deltaSpeed = _moveSpeed * Time.deltaTime;
+            var movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized * deltaSpeed;
+
+            transform.Translate(movement, Space.World);
+            
+            if (movement != Vector3.zero)
+            {
+                _animator.SetBool(_isStandingHash, false);
+                _animator.SetBool(_isWalkingHash, true);
+                _timer += Time.deltaTime;
+                Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _turnSpeed * Time.deltaTime);
+                if (_timer >= _transitionTime)
+                {
+                    _moveSpeed = 4f;
+                    _animator.SetBool(_isStandingHash, false);
+                    _animator.SetBool(_isWalkingHash, false);
+                    _animator.SetBool(_isRuningHash, true);
+                }
+            }
+            else
+            {
+                _animator.SetBool(_isWalkingHash, false);
+                _animator.SetBool(_isStandingHash, true);
+                _animator.SetBool(_isRuningHash, false);
+                _timer = 0f;
+                _moveSpeed = 2f;
+            }
+            
         }
     }
 }
